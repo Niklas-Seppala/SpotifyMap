d508c99264fa104cfed9b3528673af609f557fed
Niklas Seppala <not@working.com>
2022-04-11 19:07:55 +0300

    Initial Commit

---
+++ SpotifyMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpotifyMap
+//
+//  Created by iosdev on 11.4.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotifyMap/SpotifyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyMapApp.swift
+//  SpotifyMap
+//
+//  Created by iosdev on 11.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
